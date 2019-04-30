b86ed015a02a0b087de5a258e997f51d33a33514
Dmitry Dorofeev <dorofeev@game-insight.com>
2019-04-30 20:16:55 +0300

    added request/response logging

--- uhttp/core.py
+++ uhttp/core.py
@@ -1,4 +1,5 @@
 import json
+import traceback
 from time import time
 from urllib.parse import parse_qsl
 
@@ -8,7 +9,7 @@ from .logging import create_logger
 
 class Request:
     def __init__(self, wsgi_environ):
-        self.init_time = time()
+        self.create_time = time()
         self.body = None
         self._wsgi_environ = wsgi_environ
 
@@ -28,19 +29,35 @@ class Request:
                 self.GET[name] = value
 
     def read(self):
-        data = None
-        if self.input and self.content_length > 0:
-            data = self.input.read(self.content_length) # TODO: Whether can input be closed?
-        if not data and self.body:
-            return self.body
-        self.body = data
-        return data
+        if not self.body and self.input and self.content_length > 0:
+            self.body = self.input.read(self.content_length) # TODO: Whether can input be closed?
+        return self.body
 
     def json(self):
         if not self.body:
             self.read()
         data = self.body.decode()
         return json.loads(data)
+
+    def __to_dict__(self):
+        body = (self.body and self.body.decode()) or self.read()  # TODO: to check `Content-Type` header. If it is `application/octet-stream`, then to convert data to base64.
+        data = {
+            'create_time': self.create_time,
+            'body': body,
+        }
+        for name, value in self._wsgi_environ.items():
+            if isinstance(value, str) or isinstance(value, int):
+                data[name] = value
+        return data
+
+    def __str__(self):
+        return self.__unicode__()
+
+    def __unicode__(self):
+        return json.dumps(self.__to_dict__())
+
+    # def __repr__(self):
+    #     return f'Request({self._wsgi_environ})'
 
 
 class Response:
@@ -53,21 +70,50 @@ class Response:
     }
 
     def __init__(self, body, status_code=200, headers=None, *, exception=None):
-        self.init_time = time()
+        self.create_time = time()
         self.body = body if body else b''
-        if headers is None:
-            headers = [
-                ('Content-Type', 'text/plain'),
-                ('Content-Length', str(len(self.body))),
-            ]
-        self.headers = headers
         if isinstance(self.body, str):
-            self.body = self.body.encode() 
+            self.body = self.body.encode()
+        elif isinstance(self.body, bytes):
+            pass
+        else:
+            raise Exception('Parameter `body` must have `str` or `bytes` type.')
+        content_length = len(self.body)
+        response_headers = []
+        if content_length > 0:
+            response_headers.append(('Content-Length', str(content_length)))
+        if headers:
+            for name, value in headers:
+                response_headers.append((name, value))
+        else:
+            response_headers.append(('Content-Type', 'text/plain'))
+        self.headers = response_headers
         status_message = self.statuses.get(status_code, 'Unknown')
         self.status_code = status_code
-        self.status = '{0} {1}'.format(status_code, status_message)
+        self.status = f'{status_code} {status_message}'
         self.exception = exception
 
+    def __to_dict__(self):
+        body = self.body.decode()  # TODO: to check `Content-Type` header. If it is `application/octet-stream`, then to convert data to base64.
+        data = {
+            'create_time': self.create_time,
+            'body': body,
+            'headers': self.headers,
+            'status': self.status,
+        }
+        if self.exception:
+            tb = self.exception.traceback
+            data['exception'] = traceback.format_tb(tb)
+        return data
+
+    def __str__(self):
+        return self.__unicode__()
+
+    def __unicode__(self):
+        return json.dumps(self.__to_dict__())
+
+    # def __repr__(self):
+    #     return f'Response({self.body}, {self.status_code}, {self.headers}, exception={self.exception})'
 
 class HTTPFound(Response):
     def __init__(self, redirect_url):
@@ -110,8 +156,23 @@ class WsgiApplication:
         return response.status, response.headers, response.body
 
     def log_request(self, request, response):
+        message = {
+            'ts': time(),
+            'request': request.__to_dict__(),
+            'response': response.__to_dict__(),
+        }
+        first_digit = response.status_code // 100
         if response.exception:
-            print(response.exception)
+            level = 'fatal'
+        elif first_digit == 5:
+            level = 'error'
+        elif first_digit == 4:
+            level = 'warning'
+        else:
+            level = 'info'
+        message['level'] = level
+        log_function = getattr(self.logger, level)
+        log_function(json.dumps(message))
 
     def __call__(self, environ, start_response):
         status, response_headers, response_body = self.wsgi_request(environ)
--- uhttp/logging.py
+++ uhttp/logging.py
@@ -1,6 +1,18 @@
 import logging
+import sys
+from logging import StreamHandler, Formatter
 
 
-def create_logger():
-    logger = logging.getLogger()
+class DummyFormatter(Formatter):
+    def format(self, record):
+        return record.msg
+
+
+def create_logger(log_level='WARNING'):
+    stream_handler = StreamHandler(sys.stdout)
+    formatter = DummyFormatter()
+    stream_handler.setFormatter(formatter)
+    logger = logging.getLogger('uhttp')
+    logger.setLevel(log_level)
+    logger.addHandler(stream_handler)
     return logger
