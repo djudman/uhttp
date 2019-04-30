0abba2f57f52528c2d8cd0266a1609162fe96450
Dmitry Dorofeev <dorofeev@game-insight.com>
2019-04-30 20:15:47 +0300

    smallfix for sample apps

--- examples/echoapp/httpd.py
+++ examples/echoapp/httpd.py
@@ -6,19 +6,21 @@ def handle_request(request):
     return f'{request.path}\n'
 
 
-app = WsgiApplication(
-    src_root=realpath(dirname(__file__)),
-    urls=(
-        ('GET', '^.*$', handle_request),
-    )
-)
-
-
 if __name__ == '__main__':
     from wsgiref.simple_server import make_server
+
+    app = WsgiApplication(
+        src_root=realpath(dirname(__file__)),
+        urls=(
+            ('GET', '^.*$', handle_request),
+        )
+    )
     server = make_server('127.0.0.1', 8000, app)
     print(
         'Echo app started on port 8000...\n'
         'Just do request like `curl -X GET --http1.1 http://127.0.0.1:8000/foo` and you will get `/foo` as response\n'
     )
-    server.serve_forever()
+    try:
+        server.serve_forever()
+    except KeyboardInterrupt:
+        pass
--- examples/timeapp/httpd.py
+++ examples/timeapp/httpd.py
@@ -11,17 +11,19 @@ def get_current_time(request):
     return f'{time()}\n'
 
 
-app = WsgiApplication(
-    src_root=realpath(dirname(__file__)),
-    config={'path': 'time'},
-)
-
-
 if __name__ == '__main__':
     from wsgiref.simple_server import make_server
+
+    app = WsgiApplication(
+        src_root=realpath(dirname(__file__)),
+        config={'path': 'time'},
+    )
     server = make_server('127.0.0.1', 8000, app)
     print(
         'Time app started on port 8000...\n'
         'Just do request like `curl -X GET --http1.1 http://127.0.0.1:8000/time` and you will get the current time as response\n'
     )
-    server.serve_forever()
+    try:
+        server.serve_forever()
+    except KeyboardInterrupt:
+        pass
