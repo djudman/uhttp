6744de02b97514b0b7908c2a95349e9e7aaa90ba
Dmitry Dorofeev <dorofeev@game-insight.com>
2019-07-15 13:22:53 +0300

    new shortcut `html()`, another shortcut renamed from `restricted` to `auth_required`

--- uhttp/shortcuts.py
+++ uhttp/shortcuts.py
@@ -1,8 +1,10 @@
-from .core import Request
+from pathlib import Path
+
+from .core import Request, Response
 from .errors import InvalidAuthToken
 
 
-class restricted:
+class auth_required:
     def __init__(self, method):
         self._method = method
 
@@ -11,3 +13,16 @@ class restricted:
         if token in request.app.tokens:
             return self._method(request)
         raise InvalidAuthToken(token)
+
+
+class html:
+    def __init__(self, method, filename):
+        self._method = method
+        self._filename = filename
+
+    def __call__(self, request: Request):
+        config = request.app.config
+        filepath = Path(config["html_root"], self._filename)
+        data = filepath.read_bytes()
+        return Response(data, headers=[('Content-Type', 'text/html')])
+
