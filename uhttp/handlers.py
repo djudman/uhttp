4f36ffdfb1a1da007dd35600373410327c20a7e7
Dmitry Dorofeev <dorofeev@game-insight.com>
2019-07-15 15:13:40 +0300

    one more default handler `file`. For getting file of any type

--- uhttp/handlers.py
+++ uhttp/handlers.py
@@ -1,12 +1,15 @@
+from functools import partial
 from pathlib import Path
 
 from .core import Request, Response
 from .shortcuts import _check_auth_token
 
 
-class html:
-    def __init__(self, filename, auth_required=False):
+class file:
+    def __init__(self, filename, content_type="application/octet-stream",
+                 auth_required=False):
         self._filename = filename
+        self._content_type = content_type
         self._auth_required = auth_required
 
     def __call__(self, request: Request):
@@ -15,4 +18,7 @@ class html:
         config = request.app.config
         filepath = Path(config["html_root"], self._filename)
         data = filepath.read_bytes()
-        return Response(data, headers=[('Content-Type', 'text/html')])
+        return Response(data, headers=[('Content-Type', self._content_type)])
+
+
+html = partial(file, content_type="text/html")
