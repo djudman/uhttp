6a07da4886d305a2f240bdf0c9b4596843bc6357
Dmitry Dorofeev <dorofeev@game-insight.com>
2019-07-22 18:04:11 +0300

    error handling in auth_required() shortcut

--- uhttp/auth/shortcuts.py
+++ uhttp/auth/shortcuts.py
@@ -7,27 +7,25 @@ from ..errors import InvalidAuthToken
 def auth_required(method=None, *, login_url=None):
     def wrap(original_handler):
         def handler(request):
-            token = request.http_variables.get("HTTP_AUTH_TOKEN")
-            if not token:
-                token = request.http_variables.get("HTTP_COOKIE")
-            if token:
+            try:
+                token = request.http_variables.get("HTTP_AUTH_TOKEN")
+                if not token:
+                    token = request.http_variables.get("HTTP_COOKIE")
+                if not token:
+                    raise InvalidAuthToken()
                 config = request.app.config
                 secret = config["secret"]
-                try:
-                    header, payload = verify_token(token, secret)
-                except JwtInvalidSignature as e:
-                    session_id = payload["sid"]
-                    if login_url:
-                        return HTTPFound(login_url)
-                    raise e
+                _, payload = verify_token(token, secret)
+                session_id = payload["sid"]
                 admins = config.get("admins", [])
                 username = verify_session(session_id, admins)
                 if username:
                     return original_handler(request)
-            # if token isn't set or invalid, try redirect to login page
-            if login_url:
-                return HTTPFound(login_url)
-            raise InvalidAuthToken(token)
+                raise InvalidAuthToken(token)
+            except Exception as e:
+                if login_url:
+                    return HTTPFound(login_url)
+                raise e
         return handler
 
     return wrap if method is None else wrap(method)
