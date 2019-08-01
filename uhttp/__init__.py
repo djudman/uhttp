4cd3fc4173aecde966b963b35293b5005b2a8b3c
Dmitrii Dorofeev <djudman@gmail.com>
2019-08-01 23:38:18 +0300

    fix imports

--- uhttp/__init__.py
+++ uhttp/__init__.py
@@ -1,3 +1,4 @@
+import uhttp.handlers as handlers
 from .core import WsgiApplication, Request, Response
 
-__all__ = ['WsgiApplication', 'Request', 'Response']
+__all__ = ['WsgiApplication', 'Request', 'Response', 'handlers']
